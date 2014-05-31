f6791ba2afc9efeba820a480f5394dd0bc044ff6
Patrick Reynolds <cpreynolds@gmail.com>
2014-05-31 12:04:19 -0400

    Adding the javascript to hide download links for anonymous users.

--- public/js/custom.layout.js
+++ public/js/custom.layout.js
@@ -5,8 +5,21 @@
 
 $(document).ready(function () {
     'use strict';
+
+    var hideDownloadsForAnon = function () {
+        if(json.global.logged !== "1") {
+            $('.downloadObject').hide();
+        }
+    };
+
+    // Go to the landing page.
     $('div.HeaderLogo').unbind('click').click(function () {
-        window.location = json.global.webroot + '/community';
+        window.location = json.global.webroot;
     });
+
+    // Remove download links if the user is not logged into the system.
+    hideDownloadsForAnon();
+    midas.registerCallback('CALLBACK_CORE_RESOURCE_HIGHLIGHTED',
+                           'rsnabranding', hideDownloadsForAnon);
 
 });
